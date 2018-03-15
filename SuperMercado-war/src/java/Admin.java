368fc57848eda69a5b4a2c5f34b9be52c0f51808
ceged <cyrilgaillotdrevon@gmail.com>
2018-03-15 10:19:13 +0100

    Merge origin/master
    Conflicts:
    	dist/gfdeploy/SuperMercado/SuperMercado-war_war/WEB-INF/classes/Menu.class

--- SuperMercado-war/src/java/Admin.java
+++ SuperMercado-war/src/java/Admin.java
@@ -39,7 +39,6 @@ public class Admin extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
         String jspChoix ="/MenuAdmin.jsp";
         String act=request.getParameter("action");
         
